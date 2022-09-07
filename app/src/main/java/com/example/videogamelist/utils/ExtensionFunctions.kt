c84812dc6ce4ac299f14e042bb357bdbf6e5e10d
Diego Matesanz <diegomatesanzfuente@gmail.com>
2022-09-07 13:38:58 +0200

    Added login screen

---
+++ app/src/main/java/com/example/videogamelist/utils/ExtensionFunctions.kt
@@ -0,0 +1,10 @@
+package com.example.videogamelist.utils
+
+import android.text.TextUtils
+
+object ExtensionFunctions {
+
+    fun String.isValidEmail(): Boolean {
+        return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+}
