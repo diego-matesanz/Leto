c99b699701c567da954a552dbc1922b6e7980f04
Diego Matesanz <diegomatesanzfuente@gmail.com>
2023-04-10 16:49:16 +0200

    Added retrofit depencies and util class

---
+++ app/src/main/java/com/dmatesanz/leto/utils/RetrofitUtil.kt
@@ -0,0 +1,21 @@
+package com.dmatesanz.leto.utils
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitUtil {
+
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(ConstantsUtil.RAWG_BASE_URL + ConstantsUtil.RAWG_API_KEY)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("X-RapidAPI-Key", ConstantsUtil.RAPID_API_KEY)
+                    .addHeader("X-RapidAPI-Host", ConstantsUtil.RAPID_API_HOST)
+                chain.proceed(request.build())
+            }.build())
+            .build()
+    }
+}
